f4c82723552524b2d4072f2f4b0a2c405c50c6dd
Betelgeuse1 <avanalos@gmail.com>
2021-03-15 12:34:16 +0100

    :poop: Temporary fix for solo endpoint refreshing issue
    Previously when refreshing on /solo, the server wouldn't render because sirv couldn't find a solo.html file (that's how it works). I just added another path inside the server.js file to palliate this. Need to find a better fix.

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-import { createServer } from 'http'
+import { App } from '@tinyhttp/app'
 import sirv from 'sirv'
 
 const port = process.env.PORT || 3000
+const app = new App()
 
-createServer(sirv('dist')).listen(port)
+app.use(sirv('dist'))
+// This is a junky way to make refreshing on /solo works
+// Need to find a more generic way that could be apply to all
+// comming endpoints
+app.use('/solo', sirv('dist'))
+
+app.listen(port, console.log('Server is running'))
